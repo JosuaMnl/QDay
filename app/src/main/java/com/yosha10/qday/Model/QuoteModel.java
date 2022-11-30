07956a4eb96ced394b192ee31d7472dd6a37f582
JosuaMnl <josuamnalu@gmail.com>
2022-11-30 16:52:00 +0700

    Membuat Quote Model

---
+++ app/src/main/java/com/yosha10/qday/Model/QuoteModel.java
@@ -0,0 +1,13 @@
+package com.yosha10.qday.Model;
+
+public class QuoteModel {
+    private String text, author;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
